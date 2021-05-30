1684cacdf62aea27325d3c2462b5a78caf5dd55a
SharmilaS22 <sharmilasureshkumar22@gmail.com>
2021-05-30 09:28:34 +0530

    read/write/list/delte operation

---
+++ app.py
@@ -0,0 +1,115 @@
+import os
+from flask import Flask, render_template, request, url_for
+from werkzeug.utils import secure_filename
+app = Flask(__name__)
+
+
+# local imports
+from s3_handler import upload_file_handler, list_files_handler,get_file_handler,delete_file_handler
+
+app.config["UPLOAD_FOLDER"] = "static/"
+
+# page to upload
+@app.route('/upload')
+def upload_file():
+    return render_template('index.html')
+	
+# api to handle uploaded file
+@app.route('/api/upload', methods = ['GET', 'POST'])
+def save_file():
+    if request.method == 'POST':
+        f = request.files['file']
+
+        f.save(secure_filename(f.filename))
+
+        res = upload_file_handler(f"{f.filename}", f.filename)
+
+        return {"result" : res}
+
+		
+# api to get all file names
+@app.route('/api/list/files', methods = ['GET'])
+def get_all_files():
+
+    all_files = list_files_handler()
+
+    return {
+        "files": all_files
+    }
+
+
+@app.route('/api/get/file', methods = ['GET'])
+def get_all_files():
+
+    result = get_file_handler()
+
+    result.save(secure_filename(result))
+
+    return {
+        "result": result
+    }
+
+@app.route('/api/delete/file', methods = ['GET'])
+def delete_files():
+
+    result = delete_file_handler()
+
+    return {
+        "result": result
+    }
+
+
+
+
+# @app.route('/get-info', methods = ['GET', 'POST'])
+# def get_info():
+#     if request.method == 'POST':
+
+#         f = request.files['file']
+        
+#         f.save(secure_filename(f.filename))
+        
+#         file = open(f.filename,"r")
+#         line_count = 0
+#         word_count = 0
+
+#         content = file.read()
+#         line_list = content.split("\n")
+        
+#         for line in line_list:
+#             word_list = line.split(" ")
+#             word_count += len(word_list)
+            
+#         for i in line_list:
+#             if i:
+#                 line_count += 1
+		
+#         print(word_count-1, len(line_list)-1)
+
+#         print("This is the number of lines in the file")
+#         print(line_count)
+
+#         print("This is the number of words in the file")
+#         print(word_count)
+
+#         return {
+#             'line count': line_count,
+#             'word count': word_count
+#         }
+
+	
+# @app.route('/image', methods = ['GET', 'POST'])
+# def display_image():
+#     if request.method == 'POST':
+#         f = request.files['file']
+
+#         filename = secure_filename(f.filename)
+#         print('hhshh---------',  app.config['UPLOAD_FOLDER'] + filename)
+#         f.save(app.config['UPLOAD_FOLDER'] + filename)
+
+#         return render_template('image-display.html', filename = f.filename )
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug = True)
+
---
+++ s3_handler.py
@@ -0,0 +1,84 @@
+import os
+
+import boto3
+from botocore.client import Config
+from decouple import config
+from boto3 import client
+
+
+AWS_ACCESS_KEY_ID     = config("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY")
+REGION_NAME           = config("REGION_NAME")
+BUCKET_NAME           = config("BUCKET_NAME")
+
+
+
+client = boto3.client(
+    's3',
+    aws_access_key_id = AWS_ACCESS_KEY_ID,
+    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+    region_name = REGION_NAME
+)
+    
+# Creating the high level object oriented interface
+resource = boto3.resource(
+    's3',
+    aws_access_key_id = AWS_ACCESS_KEY_ID,
+    aws_secret_access_key = AWS_SECRET_ACCESS_KEY,
+    region_name = REGION_NAME 
+)
+
+bucket = resource.Bucket(BUCKET_NAME)
+
+
+
+def upload_file_handler(file_path, file_name):
+    """
+    Function to upload a file to an S3 bucket
+    """
+
+    bucket.upload_file(file_path, file_name)
+
+    return "Uploaded Sucessfully"
+
+
+def list_files_handler():
+    """
+    Function to list files in a given S3 bucket
+    """
+    contents = []
+    for item in client.list_objects(Bucket=BUCKET_NAME)['Contents']:
+        contents.append(item.Key)
+
+    return contents
+
+
+def get_file_handler(file_name):
+    """
+    Function to get object from bucket
+    """
+
+    response = client.get_object(Bucket = BUCKET_NAME, Key = file_name)
+
+    return response['Body']
+
+
+def delete_file_handler(file_name):
+    """
+    Function to delete object from bucket
+    """
+
+    response = client.delete_object(Bucket = BUCKET_NAME, Key = file_name)
+    
+    return response['DeleteMarker']
+
+def download_file(file_name, bucket):
+    """
+    Function to download a given file from an S3 bucket
+    """
+    s3 = boto3.resource('s3')
+    output = f"downloads/{file_name}"
+    s3.Bucket(bucket).download_file(file_name, output)
+
+    return output
+
